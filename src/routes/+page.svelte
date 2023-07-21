cc31ad7dc7ed9510de88003e813596e61d2b0988
utxo <github@utxo.one>
2023-07-21 16:50:51 -0400

    login with nostr

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,97 @@
-<script>
-	function handleLogin() {
-	  if (window.nostr && window.nostr.getPublicKey) {
-		const publicKey = window.nostr.getPublicKey();
-		console.log('Public Key:', publicKey);
-	  } else {
-		console.log('window.nost.getPublicKey() object not found.');
-	  }
-	}
-  </script>
+<script lang="ts">
+	import { SHA256 } from "crypto-js";
+
+	const backendUrl = import.meta.env.VITE_BACKEND_URL;
+	const loginUrl = `${backendUrl}/login`;
+
+	type NostrEvent = {
+    id: string;
+    pubkey: string;
+    content: string;
+    kind: number;
+    created_at: number;
+    tags: [string, string][];
+    sig?: string;
+}
+
+	async function signAndAuthorizeEvent() {
+        try {
+            if (window.nostr) {
+                const publicKey = await window.nostr.getPublicKey();
+                const currentUrl = window.location.href;
+                const eventData: any = {
+                    kind: 27235,
+                    tags: [
+                        ["u", currentUrl + "login"],
+                        ["method", "POST"],
+                    ],
+                    content: "",
+                    pubkey: publicKey,
+                    created_at: Math.floor(Date.now() / 1000),
+                };
+
+                // "id": <32-bytes lowercase hex-encoded sha256 of the serialized event data>,
+                const sortedEventData = {
+                    content: eventData.content,
+                    created_at: eventData.created_at,
+                    kind: eventData.kind,
+                    pubkey: eventData.pubkey,
+                    tags: eventData.tags,
+                };
+
+                const serializedJson = JSON.stringify(sortedEventData);
+                const hash = SHA256(serializedJson).toString();
+                const id = hash.toLowerCase();
+
+                const event: NostrEvent = {
+                    id: id,
+                    kind: 27235,
+                    tags: [
+                        ["u", currentUrl + "login"],
+                        ["method", "POST"],
+                    ],
+                    content: "",
+                    pubkey: publicKey,
+                    created_at: Math.floor(Date.now() / 1000),
+                };
+
+                const signedEvent = await window.nostr.signEvent(event);
+                // Use the signedEvent for further processing or sending to the server
+                console.log(signedEvent);
+
+                // Send the authorization request to the backend using fetch
+                const response = await fetch(loginUrl, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: `Nostr ${btoa(
+                            JSON.stringify(signedEvent)
+                        )}`,
+                    },
+                    body: JSON.stringify({ event: signedEvent }),
+                });
+
+                // Handle the authorization response
+                if (response.ok) {
+                    console.log("Successfully logged in with Nostr")
+                } else {
+                    console.log("Error logging in with Nostr");
+                }
+            } else {
+                console.log("No nostr extension available");
+            }
+        } catch (error) {
+            console.log("Error signing event");
+        }
+    }
+
+    // Call the signAndAuthorizeEvent function directly when needed
+    function login() {
+        signAndAuthorizeEvent();
+        // Additional login logic
+    }
+
+</script>
   
   <svelte:head>
 	<title>Home</title>
@@ -19,7 +103,7 @@
 		<div class="text-center">
 		  <h1 class="text-4xl text-purple-800 font-bold mb-4">Relaying.io</h1>
 		  <h2 class="text-2xl text-purple-500 mb-8">Not your relay, not your note.</h2>
-		  <button class="px-8 py-4 bg-purple-700 text-white rounded-lg" on:click={handleLogin}>
+		  <button class="px-8 py-4 bg-purple-700 text-white rounded-lg" on:click={login}>
 			Login with Nostr 🤙
 		  </button>
 		</div>
